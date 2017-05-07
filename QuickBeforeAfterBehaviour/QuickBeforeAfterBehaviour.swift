3d84f1c55e1f7edbba65eeb60a96e5680dea11b5
Giovanni Lodi <giovanni.lodi42@gmail.com>
2017-05-08 07:01:27 +1000

    Add spec to verify beforeEach and afterEach behaviour

--- QuickBeforeAfterBehaviour/QuickBeforeAfterBehaviour.swift
+++ QuickBeforeAfterBehaviour/QuickBeforeAfterBehaviour.swift
@@ -1,1 +1,32 @@
+import Quick
 
+class Spec: QuickSpec {
+    override func spec() {
+        describe("beforeEach and afterEach behaviour") {
+            beforeEach {
+                print("⭐️ top before each")
+            }
+
+            context("some context") {
+                beforeEach {
+                    print("👉 context before each")
+                }
+
+                it("example 1") { print("😊 example 1") }
+
+                it("example 2") { print("😊 example 2") }
+
+                it("example 3") { print("😊 example 3") }
+
+                afterEach {
+                    print("👉 context after each")
+                }
+            }
+
+
+            afterEach {
+                print("⭐️ top after each")
+            }
+        }
+    }
+}
