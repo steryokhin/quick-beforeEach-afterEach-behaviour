2a0c3eb09dcdf4fff467aca31dba104d24c9bba8
Giovanni Lodi <giovanni.lodi42@gmail.com>
2017-05-08 08:44:49 +1000

    Add another context to the spec to better show the behaviour

--- QuickBeforeAfterBehaviour/QuickBeforeAfterBehaviour.swift
+++ QuickBeforeAfterBehaviour/QuickBeforeAfterBehaviour.swift
@@ -1,32 +1,46 @@
 import Quick
 
 class Spec: QuickSpec {
-    override func spec() {
-        describe("beforeEach and afterEach behaviour") {
-            beforeEach {
-                print("⭐️ top before each")
-            }
+  override func spec() {
+    describe("beforeEach and afterEach behaviour") {
+      beforeEach {
+        print("⭐️ top before each")
+      }
 
-            context("some context") {
-                beforeEach {
-                    print("👉 context before each")
-                }
-
-                it("example 1") { print("😊 example 1") }
-
-                it("example 2") { print("😊 example 2") }
-
-                it("example 3") { print("😊 example 3") }
-
-                afterEach {
-                    print("👉 context after each")
-                }
-            }
-
-
-            afterEach {
-                print("⭐️ top after each")
-            }
+      context("some context") {
+        beforeEach {
+          print("👉 context before each")
         }
+
+        it("example 1") { print("😊 example 1") }
+
+        it("example 2") { print("😊 example 2") }
+
+        it("example 3") { print("😊 example 3") }
+
+        afterEach {
+          print("👉 context after each")
+        }
+      }
+
+      context("another context") {
+        beforeEach {
+          print("🍎 context before each")
+        }
+
+        it("example 1") { print("😜 example 1") }
+
+        it("example 2") { print("😜 example 2") }
+
+        afterEach {
+          print("🍎 context after each")
+        }
+      }
+
+
+      afterEach {
+        print("⭐️ top after each")
+      }
     }
+  }
 }
