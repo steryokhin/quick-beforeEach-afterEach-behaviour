5cb9baa7b538fe067ce32c62733d39785e1a16b5
Giovanni Lodi <giovanni.lodi42@gmail.com>
2017-05-16 06:57:01 +1000

    Move one afterSuite within a context

--- QuickBeforeAfterBehaviour/AdditionalBeforeSuite.swift
+++ QuickBeforeAfterBehaviour/AdditionalBeforeSuite.swift
@@ -7,8 +7,12 @@ class AdditionalBeforeSuite: QuickSpec {
                 print("😈 a before suite call you weren't aware of")
             }
 
-            afterSuite {
-                print("😈 an after suite call you weren't aware of")
+            context("a context") {
+                // I got asked if putting these calls in a context makes any
+                // difference
+                afterSuite {
+                    print("😈 an after suite call you weren't aware of")
+                }
             }
         }
     }
